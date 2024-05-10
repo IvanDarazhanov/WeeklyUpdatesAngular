67e4917c8606489ccd81f291fdf088c668ec9325
IvanDarazhanov <ivandarazhanov17a@gmail.com>
2024-05-10 11:13:55 +0300

    end of work in class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { ListViewComponent } from './list-view/list-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,CommonModule, ListViewComponent],
+  imports: [RouterOutlet, FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
